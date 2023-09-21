311734eb7f4bdeb6c2f832b2a2e68a3f1a601c1f
Ib Kunz Mathiesen <kunzmathiesen@hotmail.com>
2023-09-21 15:00:11 +0200

    Add conditional exercises

---
+++ ConditionalExercises/ConditionalExercisesTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+
+namespace ConditionalExercises;
+
+public class ConditionalExercisesTests
+{
+    private Conditional_Exercises exercise;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        exercise = new Conditional_Exercises();
+    }
+    
+    [Test]
+    public void TestEvenOrOdd()
+    {
+        Assert.AreEqual("Odd", exercise.EvenOrOdd(5));
+        Assert.AreEqual("Even", exercise.EvenOrOdd(4));
+    }
+    
+    [Test]
+    public void TestCheckNumber()
+    {
+        Assert.AreEqual("Negative", exercise.CheckNumber(-5));
+        Assert.AreEqual("Positive", exercise.CheckNumber(5));
+        Assert.AreEqual("Zero", exercise.CheckNumber(0));
+    }
+    
+    [Test]
+    public void TestIsLeapYear()
+    {
+        Assert.IsTrue(exercise.IsLeapYear(2020));
+        Assert.IsFalse(exercise.IsLeapYear(2019));
+    }
+    
+    [Test]
+    public void TestGetGrade()
+    {
+        Assert.AreEqual("B", exercise.GetGrade(85));
+        Assert.AreEqual("A", exercise.GetGrade(95));
+        Assert.AreEqual("F", exercise.GetGrade(50));
+    }
+    
+    [Test]
+    public void TestCanVote()
+    {
+        Assert.IsFalse(exercise.CanVote(17));
+        Assert.IsTrue(exercise.CanVote(18));
+    }
+
+    [Test]
+    public void TestLargestOfThree()
+    {
+        Assert.AreEqual(10, exercise.LargestOfThree(5, 10, 8));
+    }
+    
+    [Test]
+    public void TestIsDivisible()
+    {
+        Assert.IsTrue(exercise.IsDivisible(10, 5));
+        Assert.IsFalse(exercise.IsDivisible(10, 3));
+    }
+
+    [Test]
+    public void TestIsPrime()
+    {
+        Assert.IsTrue(exercise.IsPrime(7));
+        Assert.IsFalse(exercise.IsPrime(9));
+    }
+    
+    [Test]
+    public void TestIsVowel()
+    {
+        Assert.IsTrue(exercise.IsVowel('a'));
+        Assert.IsFalse(exercise.IsVowel('b'));
+    }
+    
+    [Test]
+    public void TestStartsWithCapital()
+    {
+        Assert.IsTrue(exercise.StartsWithCapital("Hello"));
+        Assert.IsFalse(exercise.StartsWithCapital("hello"));
+    }
+
+
+
+
+
+
+
+
+}
---
+++ ConditionalExercises/Conditional_Exercises.cs
@@ -0,0 +1,99 @@
+﻿namespace ConditionalExercises;
+
+public class Conditional_Exercises
+{
+    // Description: Write a method that checks if a number is even or odd.
+    // Example Input: 5
+    // Example Output: "Odd"
+    public string EvenOrOdd(int num)
+    {
+        // TODO: Implement this method
+        return "";
+    }
+    
+    // Description: Write a method that checks if a number is positive, negative, or zero.
+    // Example Input: -5
+    // Example Output: "Negative"
+    public string CheckNumber(int num)
+    {
+        // TODO: Implement this method
+        return "";
+    }
+    
+    // Description: Write a method that checks if a year is a leap year.
+    // Example Input: 2020
+    // Example Output: true
+    public bool IsLeapYear(int year)
+    {
+        // TODO: Implement this method
+        return false;
+    }
+
+    // Description: Write a method that returns the grade based on a score.
+    // 90-100: A, 80-89: B, 70-79: C, 60-69: D, 0-59: F
+    // Example Input: 85
+    // Example Output: "B"
+    public string GetGrade(int score)
+    {
+        // TODO: Implement this method
+        return "";
+    }
+
+    // Description: Write a method that checks if a person is eligible
+    // to vote based on their age (>= 18).
+    // Example Input: 17
+    // Example Output: false
+    public bool CanVote(int age)
+    {
+        // TODO: Implement this method
+        return false;
+    }
+
+    // Description: Write a method that returns the largest of three numbers.
+    // Example Input: 5, 10, 8
+    // Example Output: 10
+    public int LargestOfThree(int a, int b, int c)
+    {
+        // TODO: Implement this method
+        return 0;
+    }
+
+    // Description: Write a method that checks if one number is divisible by another.
+    // Example Input: 10, 5
+    // Example Output: true
+    public bool IsDivisible(int num, int divisor)
+    {
+        // TODO: Implement this method
+        return false;
+    }
+
+    // Description: Write a method that checks if a number is prime.
+    // Example Input: 7
+    // Example Output: true
+    public bool IsPrime(int num)
+    {
+        // TODO: Implement this method
+        return false;
+    }
+    
+    // Description: Write a method that checks if a character is a vowel.
+    // Example Input: 'a'
+    // Example Output: true
+    public bool IsVowel(char ch)
+    {
+        // TODO: Implement this method
+        return false;
+    }
+    
+    // Description: Write a method that checks if a string starts with a capital letter.
+    // Example Input: "Hello"
+    // Example Output: true
+    public bool StartsWithCapital(string str)
+    {
+        // TODO: Implement this method
+        return false;
+    }
+
+
+
+}
